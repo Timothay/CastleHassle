b7b6a15bc31e59fb8546cc20343087760bdc2b0f
Timothé VANMANSART <timothe.vanmansart@viacesi.fr>
2022-10-12 09:38:47 +0200

    enemy target wall

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyMovement : MonoBehaviour
 {
+    public GameObject hitbox;
+    public NavMeshAgent agent;
     // Start is called before the first frame update
     void Start()
     {
-        
+        hitbox = GameObject.FindGameObjectWithTag("Hitbox");
+        agent = this.GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        agent.SetDestination(hitbox.transform.position);
     }
 }
