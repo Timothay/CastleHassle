243da05fa32b7661133090b315d582be72a07a47
Timothé VANMANSART <timothe.vanmansart@viacesi.fr>
2022-10-14 15:45:24 +0200

    fix freeze position

--- Assets/Scripts/ObjectManager.cs
+++ Assets/Scripts/ObjectManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ObjectManager : MonoBehaviour
 {
+    public Rigidbody rb;
   private void OnCollisionEnter(Collision collision)
   {
     if (collision.gameObject.tag == "Enemy" || collision.gameObject.tag == "Default" || collision.gameObject.tag == "Untagged")
@@ -11,6 +12,10 @@ public class ObjectManager : MonoBehaviour
       Invoke("ChangeTag", 1f);
       Invoke("DestroyObject", 10f);
     }
+    if(collision.gameObject.tag == "leftHand" || collision.gameObject.tag == "rightHand")
+        {
+            rb.constraints &= ~RigidbodyConstraints.FreezePositionY;
+        }
   }
 
   private void ChangeTag()
