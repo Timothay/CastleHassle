577d1ec526b74f00978c6e7c60ef952de8991160
Timothé VANMANSART <timothe.vanmansart@viacesi.fr>
2022-10-14 16:21:03 +0200

    punch animation

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -7,13 +7,25 @@ public class EnemyMovement : MonoBehaviour
 {
     public GameObject hitbox;
     public NavMeshAgent agent;
+    public Animation anim;
+    public Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
         hitbox = GameObject.FindGameObjectWithTag("Hitbox");
         agent = this.GetComponent<NavMeshAgent>();
     }
-
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Hitbox")
+        {
+            rb.constraints = RigidbodyConstraints.FreezeAll;
+            this.GetComponent<EnemyMovement>().enabled = false;
+            this.GetComponent<NavMeshAgent>().enabled = false;
+            this.GetComponent<Animator>().SetBool("isWalking", false);
+            this.GetComponent<Animator>().SetBool("isAttacking", true);
+        }
+    }
     // Update is called once per frame
     void Update()
     {
