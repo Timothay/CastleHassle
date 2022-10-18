257767f60306f617b6520fcc2e0cc3a1b392de52
Gaspard <gaspard.cohidon@viacesi.fr>
2022-10-18 14:46:26 +0200

    Particles system

--- Assets/Scripts/RagdollManager.cs
+++ Assets/Scripts/RagdollManager.cs
@@ -4,67 +4,70 @@ using UnityEngine;
 using UnityEngine.AI;
 public class RagdollManager : MonoBehaviour
 {
-    public BoxCollider mainCollider;
-    public Rigidbody mainRigidBody;
-    public GameObject ThisEnemy;
-    public Animator ThisEnemyAnimator;
-    Collider[] ragdollColliders;
-    Rigidbody[] limbsRigidBodies;
-    public int timeForDespawn = 7;
-    void Start()
-    {
-        GetRagdollBits();
-        RagdollModeOff();
-    }
+  public BoxCollider mainCollider;
+  public Rigidbody mainRigidBody;
+  public GameObject ThisEnemy;
+  public Animator ThisEnemyAnimator;
+  Collider[] ragdollColliders;
+  Rigidbody[] limbsRigidBodies;
+  public int timeForDespawn = 7;
+  [SerializeField] ParticleSystem particles;
 
-    void RagdollModeOn()
-    {
-        this.GetComponent<NavMeshAgent>().enabled = false;
-        ThisEnemyAnimator.enabled = false;
-        foreach (Collider coll in ragdollColliders)
-        {
-            coll.enabled = true;
-        }
-        foreach (Rigidbody rigid in limbsRigidBodies)
-        {
-            rigid.isKinematic = false;
-        }
-        mainCollider.enabled = false;
-        mainRigidBody.isKinematic = true;
-        WaitAndDestroy();
-    }
-    void RagdollModeOff()
-    {
-        this.GetComponent<NavMeshAgent>().enabled = true;
-        foreach (Collider coll in ragdollColliders)
-        {
-            coll.enabled = false;
-        }
-        foreach(Rigidbody rigid in limbsRigidBodies)
-        {
-            rigid.isKinematic = true;
-        }
-        ThisEnemyAnimator.enabled = true;
-        mainCollider.enabled = true;
-        mainRigidBody.isKinematic = false;
-    }
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.tag == "Throwable")
-        {
-            GameObject.Find("KillCount").GetComponent<KillCountManager>().killCount += 1;
-            RagdollModeOn();
-            SoundManager.soundManager.PlayHurtSound();
-        }
-    }
+  void Start()
+  {
+    GetRagdollBits();
+    RagdollModeOff();
+  }
 
-    void WaitAndDestroy()
+  void RagdollModeOn()
+  {
+    this.GetComponent<NavMeshAgent>().enabled = false;
+    ThisEnemyAnimator.enabled = false;
+    foreach (Collider coll in ragdollColliders)
     {
-        Destroy(this.gameObject, 7);
+      coll.enabled = true;
     }
-    void GetRagdollBits()
+    foreach (Rigidbody rigid in limbsRigidBodies)
     {
-        ragdollColliders = ThisEnemy.GetComponentsInChildren<Collider>();
-        limbsRigidBodies = ThisEnemy.GetComponentsInChildren<Rigidbody>();
+      rigid.isKinematic = false;
     }
+    mainCollider.enabled = false;
+    mainRigidBody.isKinematic = true;
+    WaitAndDestroy();
+  }
+  void RagdollModeOff()
+  {
+    this.GetComponent<NavMeshAgent>().enabled = true;
+    foreach (Collider coll in ragdollColliders)
+    {
+      coll.enabled = false;
+    }
+    foreach (Rigidbody rigid in limbsRigidBodies)
+    {
+      rigid.isKinematic = true;
+    }
+    ThisEnemyAnimator.enabled = true;
+    mainCollider.enabled = true;
+    mainRigidBody.isKinematic = false;
+  }
+  private void OnCollisionEnter(Collision collision)
+  {
+    if (collision.gameObject.tag == "Throwable")
+    {
+      GameObject.Find("KillCount").GetComponent<KillCountManager>().killCount += 1;
+      RagdollModeOn();
+      SoundManager.soundManager.PlayHurtSound();
+      particles.Play();
+    }
+  }
+
+  void WaitAndDestroy()
+  {
+    Destroy(this.gameObject, 7);
+  }
+  void GetRagdollBits()
+  {
+    ragdollColliders = ThisEnemy.GetComponentsInChildren<Collider>();
+    limbsRigidBodies = ThisEnemy.GetComponentsInChildren<Rigidbody>();
+  }
 }
