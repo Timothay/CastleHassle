6cab0b3ce999a8d57e6bc8e8fa22818c31a95cd5
Timothé VANMANSART <timothe.vanmansart@viacesi.fr>
2022-10-20 14:14:42 +0200

    Update ArcherManager.cs

--- Assets/Scripts/ArcherManager.cs
+++ Assets/Scripts/ArcherManager.cs
@@ -5,26 +5,32 @@ using UnityEngine;
 public class ArcherManager : MonoBehaviour
 {
     private Animator archerAnimation;
-    public int timeToShoot = 5;
+    public float timeToShoot = 10f;
     public GameObject arrow;
     // Start is called before the first frame update
     void Start()
     {
+        this.GetComponent<Animator>().enabled = false;
         archerAnimation = this.GetComponent<Animator>();
-        archerAnimation.SetBool("isShooting", false);
-        StartCoroutine(ArcherShoot());
     }
 
     IEnumerator ArcherShoot()
     {
-        archerAnimation.SetBool("isShooting", true);
-        Instantiate(arrow, new Vector3(-17.157f, 13.83f, -34.375f), Quaternion.identity);
-        new WaitForSeconds(2.167f);      
-        archerAnimation.SetBool("isShooting", false);
+        new WaitForSeconds(2.167f);
+        Instantiate(arrow, new Vector3(-17.157f, 13.83f, -34.375f), Quaternion.Euler(0,19,0));
         yield return new WaitForSeconds(timeToShoot);
     }
     // Update is called once per frame
     void Update()
     {
+        if(timeToShoot > 0f)
+        {
+            timeToShoot -= Time.deltaTime;
+        }
+        else
+        {
+            StartCoroutine(ArcherShoot());
+            timeToShoot = 10f;
+        }
     }
 }
